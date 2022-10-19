850753f9a4f2bcba063cb5961046539673982c95
elizaibeth <seperreau@gmail.com>
2022-10-19 05:56:47 +0100

    reverted version

--- src/TagServiceProvider.php
+++ src/TagServiceProvider.php
@@ -36,7 +36,7 @@ class TagServiceProvider extends ServiceProvider
     public function boot()
     {
         AboutCommand::add('Lemur Engine Google Search Tag Package', fn () => [
-            'Version' => '1.0.2',
+            'Version' => '1.0.1',
             'Desc' => 'Fixed typo in readme',
             'Release Info' => 'https://github.com/lemurengine/lemurtag-googlesearch/releases/tag/1.0.2',
         ]);
